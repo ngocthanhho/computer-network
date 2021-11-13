ed357f6c5950150cb77bf5066bb5bcbd8b2de58f
Ngoc Thanh <64801415+ngocthanhho@users.noreply.github.com>
2021-11-13 17:18:24 +0700

    Update Server.py

--- src/Server.py
+++ src/Server.py
@@ -10,7 +10,9 @@ class Server:
 		except:
 			print("[Usage: Server.py Server_port]\n")
 		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		# regis socket name, '' = localhost
 		rtspSocket.bind(('', SERVER_PORT))
+		# maximum 5 connections
 		rtspSocket.listen(5)        
 
 		# Receive client info (address,port) through RTSP/TCP session
